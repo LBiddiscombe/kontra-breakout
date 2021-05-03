69515296a7d222e120a2ce35784e0fd5ec15b159
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2021-05-03 13:37:42 +0100

    set ball color props

--- scenes/game/ball.js
+++ scenes/game/ball.js
@@ -8,6 +8,8 @@ function createBall() {
     y: canvas.height - 140,
     radius: 8,
     anchor: { x: 0.5, y: 0.5 },
+    color: '#f0f0f1',
+    accentColor: '#d4d3d5',
     update: function () {
       this.advance()
       if (ball.x < this.radius || ball.x > canvas.width - this.radius) {
@@ -21,12 +23,12 @@ function createBall() {
     },
     render: function () {
       this.context.beginPath()
-      this.context.fillStyle = 'lightgrey'
+      this.context.fillStyle = this.accentColor
       this.context.arc(0, 0, this.radius, 0, Math.PI * 2)
       this.context.fill()
 
       this.context.beginPath()
-      this.context.fillStyle = 'white'
+      this.context.fillStyle = this.color
       this.context.arc(-1, -2, this.radius - 2, 0, Math.PI * 2)
       this.context.fill()
     },
